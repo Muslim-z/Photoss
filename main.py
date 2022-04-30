f552447397a408f3d3d5f7788676393fa646747b
Muslim-z <muslimzan095@gmail.com>
2022-04-30 16:26:23 +0300

    В названиях используется правильный регистр букв

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ from dotenv import dotenv_values
 
 if __name__ == "__main__":
 
-    TELEGRAM_TOKEN = dotenv_values('.env')['TELEGRAM_BOT_TOKEN']
-    TELEGRAM_CHANNEL_ID = dotenv_values('.env')['TELEGRAM_CHANNEL_ID']
-    DELAY = dotenv_values('.env')['DELAY']
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    telegram_token = dotenv_values('.env')['TELEGRAM_BOT_TOKEN']
+    telegram_channel_id = dotenv_values('.env')['TELEGRAM_CHANNEL_ID']
+    delay = dotenv_values('.env')['DELAY']
+    bot = telegram.Bot(token=telegram_token)
     for root, dirs, files in os.walk('images'):
         for filename in files:
             image_path = f'{root}/{filename}'
             with open(image_path, 'rb') as img:
                 image = img.read()
             bot.send_photo(
-                chat_id=TELEGRAM_CHANNEL_ID,
+                chat_id=telegram_channel_id,
                 photo=image
             )
-            time.sleep(int(DELAY))
+            time.sleep(int(delay))
