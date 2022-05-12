79d9f736590e85c4951e1e62a459cc56b2984005
Muslim-z <muslimzan095@gmail.com>
2022-05-12 19:31:26 +0300

    вывел значения в константу

--- NASA_photos.py
+++ NASA_photos.py
@@ -1,23 +1,19 @@
 import os
 import requests
+import datetime
 
+from tools import save_image
+from tools import find_extention
 from urllib.parse import urlparse
 
 from dotenv import dotenv_values
 
+EPIC_FILENAME = f'images/epic_photo{number}.png'
+NASA_FILENAME = f'images/day_photo{number}{find_extention(image["url"])}'
+COUNT = 20
 
-def find_extention(url):
-    url_parse = urlparse(url)
-    root, extention = os.path.splitext(url_parse.path)
-    return extention
-
-
-def save_image(url, image_path,params=''):
-    response = requests.get(url,params=params)
-    response.raise_for_status()
-
-    with open(image_path, 'wb') as file:
-        file.write(response.content)
+with open(image_path, 'wb') as file:
+    file.write(response.content)
 
 
 def fetch_epic_photo(nasa_api_key):
@@ -27,25 +23,25 @@ def fetch_epic_photo(nasa_api_key):
     response.raise_for_status()
 
     for number, image in enumerate(response.json()):
-        date = image['date'].replace("-", "/")
+        date = image["date"]
+        date = datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
+        date = date.strftime("%Y/%m/%d")
         epic_image_num = image['image']
         epic_image_url = f'https://api.nasa.gov/EPIC/archive/natural/'\
-            f'{date.split()[0]}/png/{epic_image_num}.png'
-        filename = f'images/epic_photo{number}.png'
-        save_image(epic_image_url, filename,params=payload)
+            f'{date}/png/{epic_image_num}.png'
+        save_image(epic_image_url, EPIC_FILENAME,params=payload)
 
 
 def fetch_day_photo(nasa_api_key):
     apod_url = f'https://api.nasa.gov/planetary/apod'
     payload = {
         'api_key': nasa_api_key,
-        'count': 20
+        'count': COUNT
     }
     response = requests.get(apod_url, params=payload)
     response.raise_for_status()
     for number, image in enumerate(response.json()):
-        filename = f'images/day_photo{number}{find_extention(image["url"])}'
-        save_image(image['url'], filename)
+        save_image(image['url'], NASA_FILENAME)
 
 if __name__ == "__main__":
     nasa_api_key = dotenv_values('.env')['NASA_API_KEY']
--- SpaceX_photos.py
+++ SpaceX_photos.py
@@ -1,22 +1,11 @@
 import os
 import requests
 
+from tools import save_image
+from tools import find_extention
 from urllib.parse import urlparse
 
-
-def save_image(url, image_path):
-    response = requests.get(url)
-    response.raise_for_status()
-
-    with open(image_path, 'wb') as file:
-        file.write(response.content)
-
-
-def find_extention(url):
-    url_parse = urlparse(url)
-    split_text = os.path.splitext(url_parse.path)
-    return split_text[1]
-
+SPACEX_FILENAME = f'images/last_launch_photo{number}{find_extention(image)}'
 
 def fetch_spacex_last_launch():
     launches_url = 'https://api.spacexdata.com/v3/launches'
@@ -25,9 +14,8 @@ def fetch_spacex_last_launch():
     image_links = response.json()[66]['links']['flickr_images']
 
     for number, image in enumerate(image_links):
-        filename = f'images/last_launch_photo{number}{find_extention(image)}'
-        save_image(image, filename)
+
+        save_image(image, SPACEX_FILENAME)
 
 if __name__ == "__main__":
-
     fetch_spacex_last_launch()
