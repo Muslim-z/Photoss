13ce64fe11e188164dcc17e76fde87cda492231e
Muslim-z <muslimzan095@gmail.com>
2022-05-19 19:34:33 +0300

    поправил код

--- SpaceX_photos.py
+++ SpaceX_photos.py
@@ -5,17 +5,19 @@ from tools import save_image
 from tools import find_extention
 from urllib.parse import urlparse
 
-SPACEX_FILENAME = f'images/last_launch_photo{number}{find_extention(image)}'
+FILE_DIR = 'images/'
+
 
 def fetch_spacex_last_launch():
-    launches_url = 'https://api.spacexdata.com/v3/launches'
+    launches_url = 'https://api.spacexdata.com/v3/launches/66'
     response = requests.get(launches_url)
     response.raise_for_status()
-    image_links = response.json()[66]['links']['flickr_images']
+    image_links = response.json()['links']['flickr_images']
 
     for number, image in enumerate(image_links):
+        filepath = f'{FILE_DIR}last_launch_photo{number}{find_extention(image)}'
+        save_image(image, filepath)
 
-        save_image(image, SPACEX_FILENAME)
 
 if __name__ == "__main__":
     fetch_spacex_last_launch()
